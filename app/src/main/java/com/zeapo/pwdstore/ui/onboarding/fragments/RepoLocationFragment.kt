89da90e95eaa0147eb4968e6ec9ea6a3c0277678
Harsh Shandilya <me@msfjarvis.dev>
2020-10-01 21:53:42 +0530

    RepoLocationFragment: remove checkExternalDirectory
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/com/zeapo/pwdstore/ui/onboarding/fragments/RepoLocationFragment.kt
+++ app/src/main/java/com/zeapo/pwdstore/ui/onboarding/fragments/RepoLocationFragment.kt
@@ -20,7 +20,6 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.zeapo.pwdstore.R
 import com.zeapo.pwdstore.UserPreference
 import com.zeapo.pwdstore.databinding.FragmentRepoLocationBinding
-import com.zeapo.pwdstore.di.Graph
 import com.zeapo.pwdstore.utils.PasswordRepository
 import com.zeapo.pwdstore.utils.PreferenceKeys
 import com.zeapo.pwdstore.utils.finish
@@ -44,11 +43,7 @@ class RepoLocationFragment : Fragment(R.layout.fragment_repo_location) {
 
     private val externalDirectorySelectAction = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         if (result.resultCode == AppCompatActivity.RESULT_OK) {
-            if (checkExternalDirectory()) {
-                finish()
-            } else {
-                createRepository()
-            }
+            finish()
         }
     }
 
@@ -96,25 +91,6 @@ class RepoLocationFragment : Fragment(R.layout.fragment_repo_location) {
         }
     }
 
-    private fun checkExternalDirectory(): Boolean {
-        if (settings.getBoolean(PreferenceKeys.GIT_EXTERNAL, false) &&
-            settings.getString(PreferenceKeys.GIT_EXTERNAL_REPO) != null) {
-            val externalRepoPath = settings.getString(PreferenceKeys.GIT_EXTERNAL_REPO)
-            val dir = externalRepoPath?.let { File(it) }
-            if (dir != null) Graph.buildStoreImpl(dir, true) else return false
-            if (dir.exists() && // The directory exists
-                dir.isDirectory && // The directory, is really a directory
-                dir.listFilesRecursively().isNotEmpty() && // The directory contains files
-                // The directory contains a non-zero number of password files
-                Graph.store.listRootPasswords().any { it.extension == "gpg" }
-            ) {
-                PasswordRepository.closeRepository()
-                return true
-            }
-        }
-        return false
-    }
-
     private fun createRepository() {
         val localDir = PasswordRepository.getRepositoryDirectory()
         runCatching {
@@ -137,10 +113,8 @@ class RepoLocationFragment : Fragment(R.layout.fragment_repo_location) {
         val externalRepo = settings.getBoolean(PreferenceKeys.GIT_EXTERNAL, false)
         val externalRepoPath = settings.getString(PreferenceKeys.GIT_EXTERNAL_REPO)
         if (externalRepo && externalRepoPath != null) {
-            if (checkExternalDirectory()) {
-                finish()
-                return
-            }
+            finish()
+            return
         }
         createRepository()
     }
